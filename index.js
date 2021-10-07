61900ea5f83c9f413e54f4ca221387216dba358b
Andrews Antwi-Addo <59352495+cUpid-py@users.noreply.github.com>
2021-10-07 06:27:23 +0000

    Initialized Express server repo

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+ 
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+ 
+app.listen(3000)
+
+const express = require("express")
+const app = express()
+
+app.get("/", function (req, res){
+    res.send("Hello World")
+})
+
+app.listen(3000)
